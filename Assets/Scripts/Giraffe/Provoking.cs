c07a18455be889ff472d4e4ec33f81295153a9ca
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2022-11-19 16:56:52 +0100

    Provoking

---
+++ Assets/Scripts/Giraffe/Provoking.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Provoking : MonoBehaviour
+{
+    [SerializeField] private float timeBetweenProvocations;
+    [SerializeField] private float provokingRadius;
+    
+    private float _lastProvocationTime;
+    void Start()
+    {
+        _lastProvocationTime = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Provocation") == 1) {
+            if (Time.time - _lastProvocationTime > timeBetweenProvocations) {
+                _lastProvocationTime = Time.time;
+                Collider[] colliders = Physics.OverlapSphere(transform.position, provokingRadius);
+                foreach (var collider in colliders) {
+                    collider.GetComponent<IProvocable>()?.Provoke(transform);
+                }
+            }
+        }
+    }
+}

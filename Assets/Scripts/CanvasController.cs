3dcabe6829071d2b840de7a0512e84dad0f0c35c
Patryk Lesiak <patryklesiak@student.agh.edu.pl>
2022-11-19 17:46:57 +0100

    Provocation Axis

---
+++ Assets/Scripts/CanvasController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour {
+    [SerializeField] private float dialogShowTime;
+    [SerializeField] private TextMeshProUGUI dialogText;
+    // Start is called before the first frame update
+
+    public void newDialog(string dialog){
+        StartCoroutine(showDialog(dialog));
+    }
+
+    IEnumerator showDialog(string dialog){
+        dialogText.SetText(dialog);
+        yield return new WaitForSecondsRealtime(dialogShowTime);
+        dialogText.SetText("");
+    }
+}

da301b1b981a5e30709918be6cc9bdb78271f041
Maciej <empieniazek@student.agh.edu.pl>
2022-11-19 18:33:18 +0100

    Standby ulepszone

--- Assets/Scripts/Enemy/States/StandByState.cs
+++ Assets/Scripts/Enemy/States/StandByState.cs
@@ -1,17 +1,25 @@
 using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Enemy.States
 {
     public class StandByState : IEnemyState
     {
+        public float moveChances;
         public StandByState()
         {
             enemyState = EnemyStates.Standby;
         }
         public override void Invoke(EnemyControler currentEnemy)
         {
-            
+            float roll = Random.Range(0f,1f);
+            if (roll <= moveChances)
+            {
+                float x = Random.Range(0, 10);
+                float z = Random.Range(0, 10);
+                currentEnemy.GoToDirection(new Vector3(x,0,z));
+            }
         }
 
 

29efdd2b8a4b54acdc52fb87a0f60f6b065eeffe
Gekon <jakubocytko@student.agh.edu.pl>
2022-11-19 18:23:58 +0100

    fix stun

--- Assets/Scripts/Enemy/States/StandByState.cs
+++ Assets/Scripts/Enemy/States/StandByState.cs
@@ -1,24 +1,19 @@
+using System;
 using UnityEngine;
 
 namespace Enemy.States
 {
     public class StandByState : IEnemyState
     {
-        private float _timer = 0;
         public StandByState()
         {
             enemyState = EnemyStates.Standby;
         }
         public override void Invoke(EnemyControler currentEnemy)
         {
-            if (_timer > currentEnemy.stunTime)
-            {
-                currentEnemy.stateManager.SetState(EnemyStates.Standby);
-                _timer = 0;
-                return;
-            }
-
-            _timer += Time.deltaTime;
+            
         }
+
+
     }
 }
